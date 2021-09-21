e464af638ef419f5044ded8d9394bd08cbb32d32
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-09-21 19:50:05 -0400

    Create ExpenseDate functional component
    - Remove js logic for date from ExpenseItem.js
    - Create ExpenseDate component and create file
    - Initial commit of ExpenseDate.css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,25 +1,19 @@
 //css can be linked to the js file by importing
-import "./ExpenseItem.css";
 //exports can be executed in two ways: default and named.
 //default can only export one component per file
 //named can export multiple components
+import "./ExpenseItem.css";
+import { ExpenseDate } from "./ExpenseDate";
+
 export const ExpenseItem = (props) => {
   //Insted of wrting variables here, write/import the data to App.js
   // const expenseDate = new Date(2021, 2, 28);
   // const expenseTitle = "Adobe Suite";
   // const expensePrice = 55.67;
 
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
