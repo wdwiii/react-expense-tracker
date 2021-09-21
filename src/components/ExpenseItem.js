d23c7e2e211e8763271bd5dd8ac41e8267fb37d8
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-09-21 13:24:21 -0400

    Update JSX to contain dynamic content
    - Declare array of objects containing expenses data
    - Pass props to ExpenseItem element
    - In component file, pass props into ExpenseItem function

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,20 @@
 //css can be linked to the js file by importing
 import "./ExpenseItem.css";
-
 //exports can be executed in two ways: default and named.
 //default can only export one component per file
 //named can export multiple components
-export const ExpenseItem = () => {
+export const ExpenseItem = (props) => {
+  //Insted of wrting variables here, write/import the data to App.js
   const expenseDate = new Date(2021, 2, 28);
   const expenseTitle = "Adobe Suite";
   const expensePrice = 55.67;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expensePrice}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
