0439b32a01099cd6548d7c6dbf820f7ad59d604d
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-09-21 12:02:55 -0400

    Import ExpenseItem.css/Add classNames to elements

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,16 @@
+//css can be linked to the js file by importing
+import "./ExpenseItem.css";
+
+//exports can be executed in two ways: default and named.
+//default can only export one component per file
+//named can export multiple components
 export const ExpenseItem = () => {
   return (
-    <div>
-      <div>March 3, 2012</div>
-      <div>
-        <div>Cost</div>
-        <div>$55.67</div>
+    <div className="expense-item">
+      <div>March 3, 2021</div>
+      <div className="expense-item__description">
+        <h2>Adobe Suite</h2>
+        <div className="expense-item__price">$55.67</div>
       </div>
     </div>
   );
