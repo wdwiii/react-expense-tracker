c48a6c5f5d6666eba97d58c3b96dfbb393a0607e
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-09-25 17:17:37 -0400

    Impot React from react dependency

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import { Expenses } from "./components/Expenses";
+import React from 'react';
+import { Expenses } from './components/Expenses/Expenses';
 
 const expenses = [
   {
-    id: "e1",
-    title: "Toilet Paper",
+    id: 'e1',
+    title: 'Toilet Paper',
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
   {
-    id: "e3",
-    title: "Car Insurance",
+    id: 'e3',
+    title: 'Car Insurance',
     amount: 294.67,
     date: new Date(2021, 2, 6),
   },
   {
-    id: "e4",
-    title: "New Desk (Wooden)",
+    id: 'e4',
+    title: 'New Desk (Wooden)',
     amount: 450,
     date: new Date(2021, 5, 12),
   },
