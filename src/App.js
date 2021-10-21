2a16bb8cd5a99d5151101bf0b1fdb6bd04c0d188
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-10-21 09:07:20 -0400

    Move expenses array inside app component

--- src/App.js
+++ src/App.js
@@ -2,29 +2,29 @@ import React from 'react';
 import { Expenses } from './components/Expenses/Expenses';
 import { NewExpense } from './components/NewExpense/NewExpense';
 
-const expenses = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 6),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
-
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 6),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   const addExpenseHandler = expense => {
     console.log(`In App.js`);
     console.log(expense);
@@ -33,7 +33,6 @@ function App() {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <h2>Let's get started!</h2>
       <Expenses items={expenses} />
     </div>
   );
