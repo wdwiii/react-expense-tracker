e2029b9c757a1ae9d9dccef2e0512272f74d62c2
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-10-21 11:07:35 -0400

    Create function to filter list items by date prop

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -12,13 +12,17 @@ export const Expenses = props => {
     setFilteredYear(filterValue);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map(expense => (
+      {filteredExpenses.map(expense => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
