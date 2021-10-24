c52e9b64e2c939505798eb1f28f58985f342d539
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-10-24 09:41:14 -0400

    Add render method content for chartBar

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,3 +1,20 @@
 import './ChartBar.css';
 
-export const ChartBar = prop => {};
+export const ChartBar = props => {
+  let barFillHeight = '0%';
+
+  if (props.max > 0) {
+    //If the max value is greater than zero, barFIllHeight is the value divided by the max value, rounded to the nearest integer, and converted to a string
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
+  }
+
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner"></div>
+      <div className="chart-fill" style={{ height: barFillHeight }}></div>
+      <div className="chart-label">{props.label}</div>
+    </div>
+  );
+};
+
+//Notes: JSX style props accepts a JavaScript object for it's value.
