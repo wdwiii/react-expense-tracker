66f7521720b6d40d468d1901470db3eea63e46a4
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-10-24 12:16:00 -0400

    Pull 'value' key from dataPoint and calc. maxValue

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,18 +1,23 @@
 import './Chart.css';
-import ChartBar from './ChartBar';
+import { ChartBar } from './ChartBar';
 
-export const Chart = prop => {
+export const Chart = props => {
+  //Return s a new array with only the value for each data point, transforming the data from objects to numbers
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+  //Max method wants list of items and not an array
+  const totalMax = Math.max(...dataPointValues);
+
   return (
     //Make the number of bars inside the Chart dynamic. Map through an array of data to generate a ChartBar for each item in the array.
     <div className="chart">
-      {props.dataPoints.map(dataPoint => {
+      {props.dataPoints.map(dataPoint => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
-          label={dadtaPoint.label}
-        />;
-      })}
+          maxValue={totalMax}
+          label={dataPoint.label}
+        />
+      ))}
     </div>
   );
 };
