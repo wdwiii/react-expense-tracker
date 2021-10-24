b064dc42c4b64355404a7365b764af794f8a926c
Willie Whitfield <71739892+wdwiii@users.noreply.github.com>
2021-10-24 12:13:52 -0400

    Reorganize ChartBart child elements
    -Correct classNames to 'chart-bar__*'

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -3,16 +3,20 @@ import './ChartBar.css';
 export const ChartBar = props => {
   let barFillHeight = '0%';
 
-  if (props.max > 0) {
+  if (props.maxValue > 0) {
     //If the max value is greater than zero, barFIllHeight is the value divided by the max value, rounded to the nearest integer, and converted to a string
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + '%';
   }
 
   return (
     <div className="chart-bar">
-      <div className="chart-bar__inner"></div>
-      <div className="chart-fill" style={{ height: barFillHeight }}></div>
-      <div className="chart-label">{props.label}</div>
+      <div className="chart-bar__inner">
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
     </div>
   );
 };
